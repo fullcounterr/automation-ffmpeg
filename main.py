5a9bf8668a9e41b81d5f946897378245d54918c7
nandadytm <fullcountra@gmail.com>
2019-10-08 14:47:56 +0700

    Add requirements.txt, watchdog and tested some other stuff

---
+++ main.py
@@ -0,0 +1,70 @@
+import os
+import argparse
+# Watchfolder
+import time
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler   
+
+DESTINATION_PATH = ""
+
+class FolderNotFoundError(Exception):
+    pass
+
+
+def on_created(event):
+    print("File {src} terdeteksi".format(src=event.src_path))
+    extension = os.path.splitext(event.src_path)[-1].lower()
+    file_name = os.path.basename(event.src_path)
+    print(extension)
+    print(file_name)
+    print(DESTINATION_PATH)
+    if extension == ".mkv" or extension == ".mp4" or extension == ".avi":
+        print("File is encodeable with ffmpeg. Waiting for 3Processing now....")
+        os.system("auto.py -input_file \"{src}\" -hevc -aq_str 0.8 -destinasi_file  \"{dst}\\{filename}_encoded{extension}\"".format(src=event.src_path, extension=extension, filename=file_name, dst=DESTINATION_PATH))
+    else:
+        print("File tidak dapat di encode. Lanjut observe...")
+        pass
+    return True
+
+def on_deleted(event):
+    pass
+
+def on_modified(event):
+    pass
+
+def on_moved(event):
+    pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-spath', type=str, help='lokasi path yang akan digunakan.')
+    parser.add_argument('-dpath', type=str, help='lokasi path yang akan digunakan.')
+    params = parser.parse_args().__dict__
+    if params['spath'] is None:
+        raise FolderNotFoundError('Folder tidak ada')
+    if params['dpath'] is None:
+        raise FolderNotFoundError('Folder tidak ada')
+    
+    DESTINATION_PATH = params['dpath']
+    # Initiate event handler for file handling
+    ignore_patterns = ""
+    ignore_directories = False
+    case_sensitive = True
+    handle_event = PatternMatchingEventHandler("*", ignore_patterns, ignore_directories, case_sensitive)
+    handle_event.on_created = on_created
+    handle_event.on_deleted = on_deleted
+    handle_event.on_modified = on_modified
+    handle_event.on_moved = on_moved
+
+    # Initiate observer
+    observer = Observer()
+    #observer.schedule(os.system("auto.py -input_file \"C:\\Kosongan\" -hevc -aq_str 0.8 -destinasi_file \"maksimal.mkv\""), params['path'], recursive=True)
+    observer.schedule(handle_event, params['spath'], recursive=False)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+        observer.join()
+    
